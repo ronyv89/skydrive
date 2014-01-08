0b8b3ff420dee0b4affd9ad6bd5aff1e9b7ca760
Rony Varghese <ronyv250289@gmail.com>
2014-01-08 11:01:44 +0530

    Changes to user

--- lib/skydrive/video.rb
+++ lib/skydrive/video.rb
@@ -1,17 +1,5 @@
 module Skydrive
   # A user's video in SkyDrive.
   class Video < Skydrive::File
-    # The link that can be used to download the video file
-    # @return [String]
-    def download_link
-      url = client.get("/#{id}/content", :download => true, :suppress_redirects => true)["location"]
-    end
-
-    # Download the video file
-    def download
-      uri = URI(download_link)
-      response = HTTParty.get("http://#{uri.host}#{uri.path}?#{uri.query}")
-      response.parsed_response
-    end
   end
 end
--- spec/skydrive/user_spec.rb
+++ spec/skydrive/user_spec.rb
@@ -5,6 +5,13 @@ describe Skydrive::User do
       name: "Rony Varghese"
     })
   }
+
+  let(:skydrive){ YAML.load(%{
+      id: "123456"
+      name: "Rony Varghese"
+    })
+
+  }
   subject { Skydrive::User.new(skydrive_test_client, user)}
   it "should return the id of the user" do
     subject.id.should == "123456"
@@ -12,5 +19,10 @@ describe Skydrive::User do
 
   it "should return the name of the user" do
     subject.name.should == "Rony Varghese"
+  end
+
+  it "should return the user's skydrive home folder" do
+    stub_request(:get, "https://apis.live.net/v5.0/123456/skydrive?access_token=access_token").to_return(:status => 200, :body => skydrive.to_json, :headers => {})
+    subject.skydrive.should == 1
   end
 end
